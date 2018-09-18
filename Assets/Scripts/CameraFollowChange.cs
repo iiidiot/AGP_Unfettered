2dd02a492fdce85be165ffa92e7b87c3b9155464
iiidiot <nothing0n@foxmail.com>
2018-09-17 19:47:08 -0700

    mod level 1

---
+++ Assets/Scripts/CameraFollowChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowChange : MonoBehaviour {
+    public int flag = 0;
+	// Use this for initialization
+	void Start () {
+        Camera.main.GetComponent<CameraFollowTight>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (flag == 0)
+            {
+                Camera.main.GetComponent<CameraFollowTight>().enabled = true;
+                Camera.main.GetComponent<CameraFollowSmooth>().enabled = false;
+            }
+            else if(flag == 1)
+            {
+                Camera.main.GetComponent<CameraFollowTight>().enabled = false;
+                Camera.main.GetComponent<CameraFollowSmooth>().enabled = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFollowTight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowTight : MonoBehaviour {
+
+    public Transform followTarget;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - followTarget.position;
+	}
+
+    // Update is called once per frame
+    void LateUpdate () {
+        //x, y 跟随
+        transform.position = new Vector3((followTarget.position + offset).x, (followTarget.position + offset).y, transform.position.z);
+    }
+}
