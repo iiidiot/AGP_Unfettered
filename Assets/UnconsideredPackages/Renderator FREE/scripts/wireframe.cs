a0611a2a998cedc0f549aeb8828f2c2dc1a71847
iiidiot <nothing0n@foxmail.com>
2018-09-19 19:12:26 -0700

    add capture for designer

---
+++ Assets/UnconsideredPackages/Renderator FREE/scripts/wireframe.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class wireframe : MonoBehaviour {
+
+	public KeyCode wireFrameKey;
+	public bool wireFrameMode;
+
+	public void Start()
+	{
+		
+	}
+
+	void OnPreRender() {
+		if(wireFrameMode)
+			GL.wireframe = true;
+		else
+			GL.wireframe = false;
+	}
+	void Update()
+	{
+		if(Input.GetKeyDown(wireFrameKey))
+			wireFrameMode = !wireFrameMode;
+	}
+}
