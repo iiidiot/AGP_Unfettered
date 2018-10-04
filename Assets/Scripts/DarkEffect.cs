19dc66cf52257da04c8a928edc1abbdac52f1c15
Yuni Zhang <yunizhan@usc.edu>
2018-10-03 21:32:19 -0700

    circle dark effect

---
+++ Assets/Scripts/DarkEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DarkEffect : MonoBehaviour
+{
+    public Transform item; // item has light
+
+    private Material caveMaterial;
+    private Renderer caveRender;
+   
+    private void OnEnable()
+    {
+        caveRender = this.GetComponent<Renderer>();
+        caveMaterial = caveRender.sharedMaterial;
+        
+    }
+
+    private void Update()
+    {
+        caveMaterial.SetVector("_ItemPos", item.position);
+    }
+
+    //private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    //{
+
+    //    caveMaterial.SetVector("_ItemPos", item.position);
+
+    //    Graphics.Blit(source, destination, caveMaterial);
+    //}
+}
