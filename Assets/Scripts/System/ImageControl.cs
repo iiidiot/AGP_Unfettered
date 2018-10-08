4ed357c341274ec55d05a379e0e67354a45dc0b6
Xuehui Zhu <xuehuizhu@Xuehuis-MacBook-Air.local>
2018-10-08 01:02:00 -0700

    talk system optimization

---
+++ Assets/Scripts/System/ImageControl.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageControl : MonoBehaviour
+{
+    // Use this for initialization
+    Vector3 previousScale;
+    Transform tranf;
+    public AnimationCurve curve;
+    public float duration = 1;
+    public bool flag = false;
+    float n;
+    //public bool fadingOut = false;
+    public Image image;
+    float counter = 0;
+    Color currentColor;
+    Color visibleColor;
+
+    void Start()
+    {
+        var tempColor = image.color;
+        tempColor.a = 0f;
+        image.color = tempColor;
+        currentColor = image.color;
+        visibleColor = image.color;
+        visibleColor.a = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag == true) {
+            Reset();
+            flag = false;
+        }
+        //if (fadingOut == true)       // fade out
+        //{
+        //    //Fully fade in Image (1) with the duration of 2
+        //    image.CrossFadeAlpha(0f, duration, false);
+        //    fadingOut = false;
+        //    resetColor();
+        //}
+        n = Time.deltaTime * 10 / duration;
+        tranf.localScale = Vector3.Lerp(this.transform.localScale, previousScale, curve.Evaluate(n));   // zoom in
+     
+        if (counter < duration) {       //fade in
+            counter += Time.deltaTime;
+            image.color = Color.Lerp(currentColor, visibleColor, counter / duration);
+        }
+    }
+
+    void Reset()
+    {
+        previousScale = this.transform.localScale;
+        tranf = GetComponent<RectTransform>();
+        transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+    }
+}
+
--- Assets/Scripts/System/NPCTalk.cs
+++ Assets/Scripts/System/NPCTalk.cs
@@ -5,52 +5,59 @@ using System.Collections;
 public class NPCTalk : MonoBehaviour
 {
     public float Y_offset = 200;
-    public float timer = 2.0f;
 
     private GameObject dialog;
+    private GameObject image;
     private string[] allLines;
     private int count = 0;
     private float tempTimer;
-    //private int timeDelay = 0;
-    //private const int gap = 300;
+    char previousTalker = 'a';
+
 
     // Use this for initialization
     void Start()
     {
         dialog = GameObject.Find("Dialog");
+        image = GameObject.Find("Image");
         string path = "Text/Talk/" + gameObject.name;
         string myTxt = ((TextAsset)Resources.Load(path)).text;
         allLines = myTxt.Split('\n');
-        tempTimer = timer;
-        //dialog.SetActive(false);
+        dialog.SetActive(false);
+        image.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    dialog.SetActive(true);
-        //}
-        tempTimer -= Time.deltaTime;
-        if (tempTimer <= 0 && count < allLines.Length)
+        if ((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.Mouse0)))
         {
-            string content = allLines[count++];
-            if (content[0] == 'p') {        // player talk
-                dialog.GetComponent<Text>().color = Color.red;
-                dialog.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(GameObject.FindGameObjectWithTag("Player").transform.position) + new Vector3(0, Y_offset, 0);
 
-            }
-            else {      // npc talk
-                dialog.GetComponent<Text>().color = Color.gray;
-                dialog.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(transform.position) + new Vector3(0, Y_offset, 0);
+            dialog.SetActive(true);
+            image.SetActive(true);
 
+            if (count < allLines.Length)
+            {
+                string content = allLines[count++];
+                if (content[0] != previousTalker) {
+                    image.GetComponent<ImageControl>().flag = true;
+                }
+                previousTalker = content[0];
+                if (content[0] == 'p')
+                {        // player talk
+                    dialog.GetComponent<Text>().color = Color.red;
+                    image.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(GameObject.FindGameObjectWithTag("Player").transform.position) + new Vector3(0, Y_offset, 0);
+
+                }
+                else
+                {      // npc talk
+                    dialog.GetComponent<Text>().color = Color.gray;
+                    image.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(transform.position) + new Vector3(0, Y_offset, 0);
+
+                }
+                content = content.Substring(2);
+                dialog.GetComponent<Text>().text = content;
             }
-            content = content.Substring(2);
-            dialog.GetComponent<Text>().text = content;
-            tempTimer = timer;
         }
-
     }
 
 }
