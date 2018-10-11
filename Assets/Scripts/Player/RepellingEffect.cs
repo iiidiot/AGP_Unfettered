3cee32efdf5270e868dfb9e79f8e22fd54f0bad7
linx777 <kingxl777@gmail.com>
2018-10-10 23:43:41 -0700

    add repelling effect on enemy

--- Assets/Scripts/Player/RepellingEffect.cs
+++ Assets/Scripts/Player/RepellingEffect.cs
@@ -10,10 +10,14 @@ public class RepellingEffect : MonoBehaviour {
 
 	[SerializeField]
 	private float repellingforce = 10000f;
+	private Animator myAnimator;
+
+	private string damageTrigger = "isDied";
 
 	void Start ()
 	{
 		myRigidbody = GetComponent<Rigidbody>();
+		myAnimator = GetComponent<Animator>();
 	}
 
 	// void OnCollisionEnter (Collision other){
@@ -39,9 +43,14 @@ public class RepellingEffect : MonoBehaviour {
 	private void GetDamage ( Collider damageoBject ){
 		// can not use position cos there is error by unity 
 		float direction = (damageoBject.GetComponent<Rigidbody>().velocity.x) > 0 ? 1 : -1;
+		myRigidbody.velocity = new Vector3(0, 0, 0);
 		myRigidbody.AddForce( direction * repellingforce,myRigidbody.velocity.y, 0);
 		PlayerController.Instance.Flip(-1 * direction);
-			
+
+		// EnemyMovementController enemyMovementController = damageoBject.gameObject.GetComponent<EnemyMovementController>();
+        // enemyMovementController.FacingRight = true;
+
+		myAnimator.SetTrigger(damageTrigger);
 	}
 
 }
