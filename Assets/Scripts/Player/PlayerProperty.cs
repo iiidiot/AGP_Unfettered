1eb2ee0c410cb819714d291afe0f73094940c13c
linx777 <kingxl777@gmail.com>
2018-10-15 17:14:37 -0700

    add map property

---
+++ Assets/Scripts/Player/PlayerProperty.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperty : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]
+	private float health = 100; 
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		damage ();
+	}
+
+	private void damage (){
+		health -= 0.0001f;
+		Debug.Log(health);
+	}
+
+
+
+}
