928d8d1ae54ec2bb7bf956cb025361791b0ba668
Yuni Zhang <yunizhan@usc.edu>
2018-10-18 02:18:20 -0700

    start button particle effect

--- Assets/StartButton.cs
+++ Assets/StartButton.cs
@@ -8,7 +8,7 @@ public class StartButton : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        effect.SetActive(false);
         this.GetComponent<Button>().onClick.AddListener(LoadNextScene);
 	}
 	
@@ -17,7 +17,20 @@ public class StartButton : MonoBehaviour {
 		
 	}
 
+    public GameObject effect;
+    //public Transform p;
+
     private void LoadNextScene()
+    {
+        //GameObject go = Instantiate(effect, p.position, p.rotation);
+
+        effect.SetActive(true);
+        effect.GetComponent<ParticleSystem>().Play();
+
+        Invoke("RealLoad", 0.5f);
+    }
+
+    private void RealLoad()
     {
         SceneManager.LoadScene("loading");
     }
