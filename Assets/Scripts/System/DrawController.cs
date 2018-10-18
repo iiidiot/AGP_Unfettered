470f463a4b38ef7e78a176d043096342c1cf4f1f
iiidiot <nothing0n@foxmail.com>
2018-10-17 23:16:27 -0700

    add effects

--- Assets/Scripts/System/DrawController.cs
+++ Assets/Scripts/System/DrawController.cs
@@ -16,7 +16,7 @@ public class DrawController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetMouseButtonDown(KeyCode.Mouse0))
         {
             if (isDrawing)
             {
