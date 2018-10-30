919ca1d3806da104890e74921ed1f4e85b727fec
Xinyu Shao <linkustarto@gmail.com>
2018-10-30 16:42:28 -0700

    add new package and change camera to otho and add fur shader

---
+++ Assets/Standard Assets/Onoty3D/CharacterParticleAsset/Scripts/CharacterParticle.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Onoty3D.CharacterParticleAsset.Scripts
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(ParticleSystem))]
+    public class CharacterParticle : MonoBehaviour
+    {
+        #region Property
+
+        [Tooltip("characters to emit")]
+        [SerializeField]
+        private string _text;
+
+        public string Text
+        {
+            get
+            {
+                return this._text;
+            }
+
+            set
+            {
+                this._text = value;
+                this._charactorIndex = 0;
+            }
+        }
+
+        [SerializeField]
+        private FontInformation _fontInformation;
+
+        public FontInformation FontInformation
+        {
+            get
+            {
+                return this._fontInformation;
+            }
+
+            set
+            {
+                this._fontInformation = value;
+            }
+        }
+
+        #endregion Property
+
+        #region Field
+
+        private ParticleSystem _particleSystem;
+        private ParticleSystemRenderer _particleSystemRenderer;
+        private int _charactorIndex = 0;
+
+        private List<Vector4> _customData = new List<Vector4>();
+
+        #endregion Field
+
+        #region Event Function
+
+        private void Start()
+        {
+            this._particleSystem = this.GetComponent<ParticleSystem>();
+            this._particleSystemRenderer = this.GetComponent<ParticleSystemRenderer>();
+
+#if UNITY_5_5
+        this._particleSystemRenderer.EnableVertexStreams(ParticleSystemVertexStreams.Custom1);
+        this._particleSystemRenderer.EnableVertexStreams(ParticleSystemVertexStreams.Custom2);
+#else //Higher Unity 5.5
+            this._particleSystemRenderer.SetActiveVertexStreams(
+            new List<ParticleSystemVertexStream>(
+            new ParticleSystemVertexStream[]
+            {
+                    ParticleSystemVertexStream.Position,
+                    ParticleSystemVertexStream.Color,
+                    ParticleSystemVertexStream.UV,
+                    ParticleSystemVertexStream.Custom1XY,
+                    ParticleSystemVertexStream.Custom1XYZW,
+            }));
+#endif
+        }
+
+        private void LateUpdate()
+        {
+            if (string.IsNullOrEmpty(this._text))
+            {
+                return;
+            }
+
+            if (this._fontInformation == null)
+            {
+                return;
+            }
+
+            var particleCount = this._particleSystem.particleCount;
+            this._particleSystem.GetCustomParticleData(this._customData, ParticleSystemCustomData.Custom1);
+            var data = default(Vector4);
+            var detail = default(FontInformation.CharacterDetail);
+            for (int i = 0; i < particleCount; i++)
+            {
+                data = this._customData[i];
+
+                //新規発生パーティクルの場合のみ文字情報セット
+                if (data.y == 0) //yの値を新規かどうかのフラグ代わりに
+                {
+                    this._charactorIndex %= this._text.Length;
+
+                    //文字情報取得
+                    this._fontInformation.TryGetCharacterDetail(this._text[this._charactorIndex], out detail);
+
+                    if (detail != null)
+                    {
+                        this._customData[i] = detail.Area;
+                    }
+
+                    this._charactorIndex++;
+                }
+            }
+            this._particleSystem.SetCustomParticleData(this._customData, ParticleSystemCustomData.Custom1);
+        }
+
+        #endregion Event Function
+
+        #region Public Method
+
+        [Obsolete("use Text property")]
+        public void SetText(string text)
+        {
+            this._text = text;
+        }
+
+        [Obsolete("use FontInformation property")]
+        public FontInformation GetFontInformation()
+        {
+            return this._fontInformation;
+        }
+
+        #endregion Public Method
+    }
+}
---
+++ Assets/Standard Assets/Onoty3D/CharacterParticleAsset/Shaders/CharacterParticleAlpha.shader
@@ -0,0 +1,104 @@
+﻿Shader "Onoty3D/CharacterParticle/Alpha" {
+	Properties{
+		_MainTex("Base (RGB) Trans (A)", 2D) = "white" {}
+		[Enum(OFF,0,FRONT,1,BACK,2)] _CullMode("Cull Mode", int) = 2 //OFF/FRONT/BACK
+	}
+
+	SubShader{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" "PreviewType" = "Plane" }
+		Blend SrcAlpha OneMinusSrcAlpha
+		ColorMask RGB
+		Cull[_CullMode]
+		Lighting Off 
+		ZWrite Off
+
+		Pass{
+
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#pragma target es3.0
+#pragma multi_compile_particles
+#pragma multi_compile_fog
+
+#include "UnityCG.cginc"
+#define R_SHIFT 12
+#define AND_VALUE 4095
+
+			struct appdata_t {
+				float4 vertex : POSITION;
+				fixed4 color : COLOR;
+				float2 texcoord : TEXCOORD0;
+				float4 custom1 : TEXCOORD1;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct v2f {
+				float4 vertex : SV_POSITION;
+				fixed4 color : COLOR;
+				float2 texcoord : TEXCOORD0;
+				float4 area : TEXCOORD1;
+				float4 region : TEXCOORD2;
+				UNITY_FOG_COORDS(1)
+				UNITY_VERTEX_OUTPUT_STEREO
+			};
+
+			uniform sampler2D _MainTex;
+			uniform float4 _MainTex_ST;
+			uniform float4 _MainTex_TexelSize;
+
+			v2f vert(appdata_t v)
+			{
+				v2f o;
+				UNITY_SETUP_INSTANCE_ID(v);
+				UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+				float4 vertex = v.vertex;
+				o.vertex = UnityObjectToClipPos(vertex);
+				o.texcoord = TRANSFORM_TEX(v.texcoord, _MainTex);
+				o.color = v.color;
+				float4 area;
+				area.x = ((int)v.custom1.x >> R_SHIFT) * _MainTex_TexelSize.x;
+				area.y = ((int)v.custom1.x & AND_VALUE) * _MainTex_TexelSize.y;
+				area.z = ((int)v.custom1.y >> R_SHIFT) * _MainTex_TexelSize.x;
+				area.w = ((int)v.custom1.y & AND_VALUE) * _MainTex_TexelSize.y;
+				o.area = area;
+				float4 region;
+				region.x = ((int)v.custom1.y >> R_SHIFT) * v.custom1.w;
+				region.y = ((int)v.custom1.y & AND_VALUE) * v.custom1.w;
+				region.z = ((int)v.custom1.z >> R_SHIFT) * v.custom1.w;
+				region.w = ((int)v.custom1.z & AND_VALUE) * v.custom1.w;
+				o.region = region;
+				UNITY_TRANSFER_FOG(o, o.vertex);
+				return o;
+			}
+
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				float2 uv = i.texcoord;
+				fixed4 col = fixed4(0, 0, 0, 0);
+
+				//メッシュの大きさの中で、実際に文字を描画したい範囲にのみ描画処理
+				if (uv.x > i.region.z
+					&& uv.x < i.region.x + i.region.z
+					&& uv.y > i.region.w
+					&& uv.y < i.region.y + i.region.w
+					)
+				{
+					//実際に文字を表示したい部分のx,yの範囲が0-1になるよう再計算
+					uv.x = i.area.x + saturate((uv.x - i.region.z) * (1.0 / i.region.x)) * i.area.z;
+					uv.y = i.area.y + saturate((uv.y - i.region.w) * (1.0 / i.region.y)) * i.area.w;
+					col = tex2Dlod(_MainTex, float4(uv, 0, 0)) * i.color;
+				}
+				else
+				{
+					discard;
+				}
+
+				UNITY_APPLY_FOG_COLOR(i.fogCoord, col, fixed4(0,0,0,0));
+				return col;
+			}
+		ENDCG
+		}
+	}
+}
