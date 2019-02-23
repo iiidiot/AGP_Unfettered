1948b552a6281bce6abddc0eb34c033f49d97d1c
Yuni Zhang <yunizhan@usc.edu>
2019-02-22 19:00:57 -0800

    add nav mesh agent

---
+++ Assets/Scripts/BehaviorDesigner/NavAgentChasePlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace BehaviorDesigner.Runtime.Tasks.Basic.UnityNavMeshAgent
+{
+    public class NavAgentChasePlayer : Action
+    {
+
+        [Tooltip("The GameObject that the task operates on. If null the task GameObject is used.")]
+        public SharedGameObject targetGameObject;
+
+        //[SharedRequired]
+        [Tooltip("The NavMeshAgent destination")]
+        public SharedGameObject playerObject;
+
+        // cache the navmeshagent component
+        private NavMeshAgent navMeshAgent;
+        private GameObject prevGameObject;
+        private GameObject playerGameObject;
+
+        public override void OnStart()
+        {
+            var currentGameObject = GetDefaultGameObject(targetGameObject.Value);
+            if (currentGameObject != prevGameObject)
+            {
+                navMeshAgent = currentGameObject.GetComponent<NavMeshAgent>();
+                prevGameObject = currentGameObject;
+            }
+
+            playerGameObject = GetDefaultGameObject(playerObject.Value);
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (navMeshAgent == null)
+            {
+                Debug.LogWarning("NavMeshAgent is null");
+                return TaskStatus.Failure;
+            }
+
+            return navMeshAgent.SetDestination(playerGameObject.transform.position) ? TaskStatus.Success : TaskStatus.Failure;
+        }
+
+        public override void OnReset()
+        {
+            targetGameObject = null;
+            playerObject = null;
+        }
+    }
+}
