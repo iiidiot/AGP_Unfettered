bd51e5dfd408fbffff608d43eee64b3446a2bd30
iiidiot <nothing0n@foxmail.com>
2019-02-26 15:54:49 -0800

    finish cave part1&2

---
+++ Assets/Scripts/Level/FallDownDie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDownDie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.transform.position = GameRunTimeStatus.RebornPlace;
+        }
+    }
+
+}
--- Assets/Scripts/StatusAndConfig/GameRunTimeStatus.cs
+++ Assets/Scripts/StatusAndConfig/GameRunTimeStatus.cs
@@ -1,6 +1,7 @@
-﻿
+﻿using UnityEngine;
 
 public static class GameRunTimeStatus{
 
     public static bool MovieMode = false;
+    public static Vector3 RebornPlace = Vector3.zero;
 }
