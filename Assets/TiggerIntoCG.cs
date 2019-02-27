b265ee0c6848d545243265666aa17dba37cd87a5
iiidiot <nothing0n@foxmail.com>
2019-02-27 05:09:07 -0800

    finish part01

---
+++ Assets/TiggerIntoCG.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class TiggerIntoCG : MonoBehaviour {
+	public PlayableDirector pd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.F))
+        {
+            pd.Play();
+        }
+    }
+}
