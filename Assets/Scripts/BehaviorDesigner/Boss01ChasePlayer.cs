51f9399d18cb126d4ce876e916b0dbabfac6aace
Yuni Zhang <yunizhan@usc.edu>
2019-03-30 19:03:45 -0700

    simple chase

---
+++ Assets/Scripts/BehaviorDesigner/Boss01ChasePlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace BehaviorDesigner.Runtime.Tasks.Basic.UnityNavMeshAgent
+{
+    public class Boss01ChasePlayer : Action
+    {
+
+        [Tooltip("The GameObject that the task operates on. If null the task GameObject is used.")]
+        public SharedGameObject targetGameObject;
+
+        //[SharedRequired]
+        [Tooltip("The NavMeshAgent destination")]
+        public SharedGameObject playerObject;
+
+        private GameObject prevGameObject;
+        private GameObject playerGameObject;
+
+        public override void OnStart()
+        {
+            var currentGameObject = GetDefaultGameObject(targetGameObject.Value);
+            if (currentGameObject != prevGameObject)
+            {
+                prevGameObject = currentGameObject;
+            }
+
+            playerGameObject = GetDefaultGameObject(playerObject.Value);
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            
+            //if(playerGameObject.transform.position.x < prevGameObject.transform.position.x)
+            //{
+                Vector3 pos = prevGameObject.transform.position;
+                prevGameObject.transform.position = new Vector3(pos.x - 0.1f, pos.y, pos.z);
+            //}
+
+            return TaskStatus.Success;
+        }
+
+        public override void OnReset()
+        {
+            targetGameObject = null;
+            playerObject = null;
+        }
+    }
+}
